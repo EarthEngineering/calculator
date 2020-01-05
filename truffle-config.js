6330d257717d744cf93d215dc0da6ea4ee335017
Gabriel Cardona <cgcardona@gmail.com>
2020-01-04 17:32:54 -0800

    Cleanup.

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 const path = require("path")
-
-module.exports = {
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+const mnemonic = (module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   // contracts_build_directory: path.join(__dirname, "src/contracts"),
@@ -9,6 +9,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/")
+      },
+      network_id: 3
     }
   },
   compilers: {
@@ -16,4 +22,4 @@ module.exports = {
       version: "0.6.1"
     }
   }
-}
+})
